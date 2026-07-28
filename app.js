efcbd99c210e6d4cd4e4010063676aa660f926a3
Lois2406 <loisayubayilgum@gmail.com>
2026-07-28 11:16:41 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("hello class");
+alert("welcome to my wibe side");
+
+let traineeName = "zuzu";
+let score = 58;
+let ispresent = true;
+
+const placeofbirth = "farin gada";
+
+console. log(typeof ispresent);
+
+console. log(traineeName);
+
+function greetuser() {
+   alert("good morning" +traineeName);
+}
+
+greetuser();
+
+let messageBtn = document.getElementById("messageBtn");
+messageBtn.addEventListener("click", function() {
+   alert ("Button clicked");
+}
+);
